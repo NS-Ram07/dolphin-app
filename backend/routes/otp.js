f6ca0558e9401ee365da98d1818a2b4075a058a6
NS-Ram07 <nalasiva2002@gmail.com>
2024-08-05 22:57:54 +0530

    push

--- backend/routes/otp.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const Router = express.Router()
-
-const otpController = require('../controller/otp.controller')
-Router.post('/create',otpController.createOtp)
-Router.post('/verify',otpController.authenticateOtp)
-
-
-module.exports = Router
